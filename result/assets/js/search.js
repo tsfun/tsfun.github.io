c0c93f4560feea962d8d5ce886c57e8a214b4040
khai96_ <hvksmr1996@gmail.com>
2020-08-26 20:19:59 +0700

    Updates

--- result/assets/js/search.js
+++
@@ -1,3 +0,0 @@
-var typedoc = typedoc || {};
-            typedoc.search = typedoc.search || {};
-            typedoc.search.data = {"kinds":{"65536":"Type literal"},"rows":[{"id":0,"kind":65536,"name":"__type","url":"interfaces/matchhandlers.html#ok.__type-1","classes":"tsd-kind-type-literal tsd-parent-kind-property","parent":"MatchHandlers.ok"},{"id":1,"kind":65536,"name":"__type","url":"interfaces/matchhandlers.html#err.__type","classes":"tsd-kind-type-literal tsd-parent-kind-property","parent":"MatchHandlers.err"}]};
